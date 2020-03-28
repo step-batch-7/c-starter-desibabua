01ed48cd8b2e85eb18474c9ae7e06de31ce5f74d
ayush <58028488+desibabua@users.noreply.github.com>
2020-03-28 19:11:16 +0530

    added is_odd funtion

--- lib/basic_functions.c
+++ lib/basic_functions.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int is_even(int);
+int is_odd(int);
 
 int is_even(int num)
 {
 	return num % 2 == 0 ? 1 : 0;
+}
+
+int is_odd(int num)
+{
+	return !is_even(num);
 }
 
 int main(void)
@@ -13,5 +19,6 @@ int main(void)
 	printf("Enter a number :\n");
 	scanf("%d",&number);
 	printf("%d is %s.\n",number,(is_even(number)?"even":"not even"));
+	printf("%d is %s.\n",number,(is_odd(number)?"odd":"not odd"));
 	return 0;
 }

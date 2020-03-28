fa4b3b18cb35fe74ae8fe8d9c747e5ed24b10185
ayush <58028488+desibabua@users.noreply.github.com>
2020-03-28 19:17:39 +0530

    added square function

--- lib/basic_functions.c
+++ lib/basic_functions.c
@@ -2,6 +2,7 @@
 
 int is_even(int);
 int is_odd(int);
+int square(int);
 
 int is_even(int num)
 {
@@ -13,12 +14,19 @@ int is_odd(int num)
 	return !is_even(num);
 }
 
+int square(int num)
+{
+	return num * num;
+}
+
 int main(void)
 {
 	int number;
 	printf("Enter a number :\n");
 	scanf("%d",&number);
+	printf("\n");
 	printf("%d is %s.\n",number,(is_even(number)?"even":"not even"));
 	printf("%d is %s.\n",number,(is_odd(number)?"odd":"not odd"));
+	printf("square of %d is %d.\n",number,square(number));
 	return 0;
 }
